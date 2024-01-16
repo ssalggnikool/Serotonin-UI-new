57160126322e656badcc14b57488f1247a06d788
Samara <97859147+ssalggnikool@users.noreply.github.com>
2024-01-15 18:20:49 -0800

    finish.

--- barracuta/UI/Tabs/ViewsInOptions/LicensesViewController.swift
+++ barracuta/UI/Tabs/ViewsInOptions/LicensesViewController.swift
@@ -12,10 +12,15 @@ import UIKit
 class LicensesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tableView: UITableView!
+    var fileNames: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Licenses"
+        
+        if let mdFiles = try? FileManager.default.contentsOfDirectory(atPath: Bundle.main.bundlePath).filter({ $0.hasSuffix(".md") }) {
+            fileNames = mdFiles
+        }
         
         tableView = UITableView(frame: .zero, style: .grouped)
         tableView.dataSource = self
@@ -32,11 +37,61 @@ class LicensesViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return fileNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = fileNames[indexPath.row]
+        return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let selectedFileName = fileNames[indexPath.row]
+        
+        if let fileContents = loadFileContents(fileName: selectedFileName) {
+            let textViewController = TextViewController()
+            textViewController.textContent = fileContents
+            textViewController.titleText = selectedFileName
+            navigationController?.pushViewController(textViewController, animated: true)
+        }
+    }
+    
+    private func loadFileContents(fileName: String) -> String? {
+        guard let filePath = Bundle.main.path(forResource: fileName, ofType: ""),
+              let fileContents = try? String(contentsOfFile: filePath) else {
+            return nil
+        }
+        return fileContents
+    }
+
+
+}
+
+class TextViewController: UIViewController {
+    
+    var textContent: String?
+    var titleText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = titleText
+        let textView = UITextView()
+        textView.text = textContent
+        textView.isEditable = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let monospacedFont = UIFont.monospacedSystemFont(ofSize: 12.0, weight: .regular)
+        textView.font = monospacedFont
+        
+        view.addSubview(textView)
+        
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.topAnchor),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
